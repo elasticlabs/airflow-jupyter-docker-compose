3cc3e27e501ca58cc566edbbe502f189a9826e4b
elasticlabs <patrice.mauny@gmail.com>
2021-03-22 17:07:46 +0100

    Huge updates on airflow Dockerfile

--- postgres/09_postgres-airflow.sh
+++ postgres/09_postgres-airflow.sh
@@ -4,25 +4,7 @@ set -e
 #
 # Create the "airflow" database and grant all access to "postgres" user 
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
+    CREATE ROLE $AIRFLOW_USER LOGIN PASSWORD '$AIRFLOW_PASSWORD';
     CREATE DATABASE $AIRFLOW_DB;
-    GRANT ALL PRIVILEGES ON DATABASE $AIRFLOW_DB TO $POSTGRES_USER;
-EOSQL
-
-#
-# Create role "airflow" if needed and grant it all accesses to "airflow" database
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$AIRFLOW_DB" <<-EOSQL
-    DO
-        $do$
-        BEGIN
-        IF NOT EXISTS (
-            SELECT                       -- SELECT list can stay empty for this
-            FROM   pg_catalog.pg_roles
-            WHERE  rolname = '$AIRFLOW_USER') THEN
-
-            CREATE ROLE $AIRFLOW_USER LOGIN PASSWORD '$AIRFLOW_PASSWORD';
-            GRANT ALL PRIVILEGES ON DATABASE '$AIRFLOW_DB' TO '$AIRFLOW_USER';
-            -- ALTER ROLE '$AIRFLOW_USER' SET search_path = airflow, geoserver, postgres;
-        END IF;
-        END
-        $do$;
+    GRANT ALL PRIVILEGES ON DATABASE $AIRFLOW_DB TO $AIRFLOW_USER;
 EOSQL
--- postgres/10_geoserver.sh
+++ postgres/10_geoserver.sh
@@ -6,8 +6,9 @@ set -e
 # TODO : proper tables rights management : https://docs.bitnami.com/aws/infrastructure/postgresql/configuration/create-postgis-template/
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
     CREATE DATABASE postgis_template IS_TEMPLATE true;
+    CREATE ROLE $GEOSERVER_USER LOGIN PASSWORD '$GEOSERVER_PASSWORD';
     CREATE DATABASE $GEOSERVER_DB;
-    GRANT ALL PRIVILEGES ON DATABASE $GEOSERVER_DB TO $POSTGRES_USER;
+    GRANT ALL PRIVILEGES ON DATABASE $GEOSERVER_DB TO $GEOSERVER_USER;
 EOSQL
 
 # Load PostGIS into both template_database and $GEOSERVER_DB
@@ -19,21 +20,5 @@ for DB in postgis_template "$GEOSERVER_DB"; do
 		CREATE EXTENSION IF NOT EXISTS fuzzystrmatch;
 		CREATE EXTENSION IF NOT EXISTS postgis_tiger_geocoder;
         CREATE EXTENSION IF NOT EXISTS pgrouting;
-EOSQL
+	EOSQL
 done
-
-# Create and grant access rights to user $GEOSERVER_DB
-"${psql[@]}" --dbname="$GEOSERVER_DB" <<-'EOSQL'
-		DO
-        $do$
-        BEGIN
-        IF NOT EXISTS (
-            SELECT                       -- SELECT list can stay empty for this
-            FROM   pg_catalog.pg_roles
-            WHERE  rolname = '$GEOSERVER_USER') THEN
-                CREATE ROLE $GEOSERVER_USER LOGIN PASSWORD '$GEOSERVER_PASSWORD';
-                GRANT ALL PRIVILEGES ON DATABASE $GEOSERVER_DB TO $GEOSERVER_USER;
-        END IF;
-        END
-        $do$;
-EOSQL
